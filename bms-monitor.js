580b8e745085287408291a88631853801e821d34
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-13 15:25:53 +0530

    breaking down production and test code

--- bms-monitor.js
+++ bms-monitor.js
@@ -14,5 +14,4 @@ function batteryIsOk(temperature, soc, charge_rate) {
     return true;
 }
 
-expect(batteryIsOk(25, 70, 0.7)).to.be.true;
-expect(batteryIsOk(50, 85, 0)).to.be.false;
+module.exports={ batteryIsOK}
