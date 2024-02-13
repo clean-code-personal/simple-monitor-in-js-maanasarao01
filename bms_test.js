dea3af1e775116b015f62324eec90917d657e465
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-13 16:46:01 +0530

    Included all possible test cases

--- bms_test.js
+++ bms_test.js
@@ -1,5 +1,18 @@
 const {expect}=require('chai')
 const {batteryIsOk}=require('\bms-monitor.js')
+const range=require('\limitsManual.js')
 
-expect(batteryIsOk(25, 70, 0.7)).to.be.true;
-expect(batteryIsOk(50, 85, 0)).to.be.false;
+//thermal check
+expect(batteryIsOk(-10, 70, 0.7,range)).to.be.false;
+expect(batteryIsOk(50, 70, 0.7,range)).to.be.false;
+//stateOfCharge error
+expect(batteryIsOk(10, 10, 0.7,range)).to.be.false;
+expect(batteryIsOk(81, 70, 0.7,range)).to.be.false;
+//chargeRate check
+expect(batteryIsOk(40, 78, -2,range)).to.be.false;
+expect(batteryIsOk(25, 50, 0.85,range)).to.be.false;
+
+//All Is Ok check
+expect(batteryIsOk(25, 70, 0.7,range)).to.be.true;
+expect(batteryIsOk(10, 20, 0.7,range)).to.be.true;
+
