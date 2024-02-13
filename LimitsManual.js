4b6cf376e77e3330a0cf5cb3b7194c18e8e1d832
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-13 15:49:41 +0530

    Create LimitsManual.js

---
+++ LimitsManual.js
@@ -0,0 +1,18 @@
+
+//An object that keeps track of limits
+const range={
+  temperature:{
+    low:0,
+    high:45
+  },
+  stateOfCharge:{
+    low:20,
+    high:80
+  },
+  chargeRate:{
+    low:0,
+    high:0.8
+  }
+};
+
+module.exports={range}
