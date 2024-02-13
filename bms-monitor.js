d909270601293376cbce6d74f9ddceb3cd474214
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-13 16:38:03 +0530

    Updated production code

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,15 +1,32 @@
-function batteryIsOk(temperature, soc, charge_rate) {
-    if (temperature < 0 || temperature > 45) {
-        console.log('Temperature is out of range!');
-        return false;
-    } else if (soc < 20 || soc > 80) {
-        console.log('State of Charge is out of range!')
-        return false;
-    } else if (charge_rate > 0.8) {
-        console.log('Charge rate is out of range!');
-        return false;
-    }
-    return true;
+const range=require('\LimitsManual.js');
+
+//This function will return a boolean value based on the range state
+//It will return true only if the inputParameter is in specified range
+function rangeState(inputParameter,rangeParameter){
+    return inputParameter>=rangeParameter.low && inputParameter<=rangeParameter.high
+}
+
+function checkBatteryCondition(batteryCondition){
+    return batteryCondition;
+}
+
+function printMishap(parameterState,mishapParameter){
+    message =parameterState? mishapParameter+'is good:)': mishapParameter+'is out of Range:(';
+    console.log(message)
+}
+
+function batteryIsOk(temperature, stateOfCharge, charge_rate,range) {
+
+    const temperatureRange=rangeState(temperature,range.temperature)
+    const stateOfChargeRange=rangeState(stateOfCharge,range.stateOfCharge)
+    const chargeRateRange=rangeState(charge_rate,range.chargeRate)
+
+    printMishap(temperatureRange,'Temperature')
+    printMishap(stateOfChargeRange,'State Of Charge')
+    printMishap(chargeRateRange,'Charge Rate')
+        
+    return checkBatteryCondition(temperatureRange && stateOfChargeRange && chargeRateRange) 
+
 }
 
 module.exports={ batteryIsOk}
