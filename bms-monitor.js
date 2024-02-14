ebf43c7ce24cf6ffddff084c80d3718d8dcc24ed
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-14 22:34:04 +0530

    Update bms-monitor.js

--- bms-monitor.js
+++ bms-monitor.js
@@ -4,33 +4,40 @@ function parameterIsInRange(inputParameter,rangeParameter){
     return inputParameter>=rangeParameter.low && inputParameter<=rangeParameter.high
 }
 
-function checkTolerance(parameterIsInRange){
-    const tolerance=rangeParameter.tolerance
-    return inputParameter>=rangeParameter.high-tolerance && inputParameter<=rangeParameter.high?'peak':inputParameter>=rangeParameter.low && inputParameter<=rangeParameter.low+tolerance?'discharge':'all is well'
+function checkTolerance(parameterIsInRange) {
+    return function() {
+        const tolerance = rangeParameter.tolerance; // Using rangeParameter here
+        return inputParameter >= rangeParameter.high - tolerance && inputParameter <= rangeParameter.high ? 'peak' :
+            inputParameter >= rangeParameter.low && inputParameter <= rangeParameter.low + tolerance ? 'discharge' :
+            'all is well';
+    };
 }
 
-function checkBatteryCondition(batteryCondition){
-    console.log( batteryCondition? 'Battery is GOOD:)\n': 'Battery is RISKY:( \tAttention needed!\n\n' )
-    return batteryCondition;
+function printMishap(parameterState, mishapParameter) {
+    const toleranceLevel = checkTolerance(parameterIsInRange)();
+    const message = parameterState ?
+        (toleranceLevel === 'peak' ? `${mishapParameter} is approaching Peak value[!]` :
+            toleranceLevel === 'discharge' ? `${mishapParameter} is approaching discharge[!]` :
+                `${mishapParameter} is good`) :
+        `${mishapParameter} is out of Range:(`;
+    console.log(message);
 }
 
-function printMishap(parameterState,mishapParameter){
-    message = parameterState? (checkTolerance(parameterIsInRange)=='peak'?mishapParameter+' is approaching Peak value[!]':checkTolerance(parameterIsInRange)=='dicharge'?mishapParameter+' is approaching discharge[!]':mishapParameter+' is good'): mishapParameter+' is out of Range:(';
-    console.log(message)
+function batteryIsOk(temperature, stateOfCharge, charge_rate, range) {
+    const temperatureIsOk = parameterIsInRange(temperature, range.temperature);
+    const stateOfChargeIsOk = parameterIsInRange(stateOfCharge, range.stateOfCharge);
+    const chargeRateIsOk = parameterIsInRange(charge_rate, range.chargeRate);
+
+    // Create functions that capture the current state of temperature, stateOfCharge, and charge_rate
+    const temperatureCheckTolerance = checkTolerance(parameterIsInRange);
+    const stateOfChargeCheckTolerance = checkTolerance(parameterIsInRange);
+    const chargeRateCheckTolerance = checkTolerance(parameterIsInRange);
+
+    printMishap(temperatureIsOk, 'Temperature');
+    printMishap(stateOfChargeIsOk, 'State Of Charge');
+    printMishap(chargeRateIsOk, 'Charge Rate');
+
+    return checkBatteryCondition(temperatureIsOk && stateOfChargeIsOk && chargeRateIsOk);
 }
 
-function batteryIsOk(temperature, stateOfCharge, charge_rate,range) {
-
-    const temperatureIsOk=parameterIsInRange(temperature,range.temperature)
-    const stateOfChargeIsOk=parameterIsInRange(stateOfCharge,range.stateOfCharge)
-    const chargeRateIsOk=parameterIsInRange(charge_rate,range.chargeRate)
-
-    printMishap(temperatureIsOk,'Temperature')
-    printMishap(stateOfChargeIsOk,'State Of Charge')
-    printMishap(chargeRateIsOk,'Charge Rate')
-        
-     return checkBatteryCondition(temperatureIsOk && stateOfChargeIsOk && chargeRateIsOk) 
-
-}
-
-module.exports={ batteryIsOk}
+module.exports = { batteryIsOk };
