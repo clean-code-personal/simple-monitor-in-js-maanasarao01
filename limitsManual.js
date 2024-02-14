082fad441ad0b5704efa53178e91a7bcfef35904
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-14 21:42:05 +0530

    added tolerance property

--- limitsManual.js
+++ limitsManual.js
@@ -3,15 +3,18 @@
 const range={
   temperature:{
     low:0,
-    high:45
+    high:45,
+    tolerance:2.25
   },
   stateOfCharge:{
     low:20,
-    high:80
+    high:80,
+    tolerance:4
   },
   chargeRate:{
     low:0,
-    high:0.8
+    high:0.8,
+    tolerance:0.032
   }
 };
 
