d6f14dea6eb5f14ac0d43629f32f13a4fbf06e4f
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-14 13:10:33 +0530

    tried to improve naming conventions

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,6 +1,6 @@
 //This function will return a boolean value based on the range state
 //It will return true only if the inputParameter is in specified range
-function rangeState(inputParameter,rangeParameter){
+function parameterIsInRange(inputParameter,rangeParameter){
     return inputParameter>=rangeParameter.low && inputParameter<=rangeParameter.high
 }
 
@@ -16,15 +16,15 @@ function printMishap(parameterState,mishapParameter){
 
 function batteryIsOk(temperature, stateOfCharge, charge_rate,range) {
 
-    const temperatureRange=rangeState(temperature,range.temperature)
-    const stateOfChargeRange=rangeState(stateOfCharge,range.stateOfCharge)
-    const chargeRateRange=rangeState(charge_rate,range.chargeRate)
+    const temperatureIsOk=parameterIsInRange(temperature,range.temperature)
+    const stateOfChargeIsOk=parameterIsInRange(stateOfCharge,range.stateOfCharge)
+    const chargeRateIsOk=parameterIsInRange(charge_rate,range.chargeRate)
 
-    printMishap(temperatureRange,'Temperature')
-    printMishap(stateOfChargeRange,'State Of Charge')
-    printMishap(chargeRateRange,'Charge Rate')
+    printMishap(temperatureIsOk,'Temperature')
+    printMishap(stateOfChargeIsOk,'State Of Charge')
+    printMishap(chargeRateIsOk,'Charge Rate')
         
-     return checkBatteryCondition(temperatureRange && stateOfChargeRange && chargeRateRange) 
+     return checkBatteryCondition(temperatureIsOk && stateOfChargeIsOk && chargeRateIsOk) 
 
 }
 
