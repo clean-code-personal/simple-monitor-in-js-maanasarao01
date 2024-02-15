cc9b247b1b67e640375de9ca6e45c8113ff875f0
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-15 19:46:06 +0530

    Update translations.js

--- production_code/translations.js
+++ production_code/translations.js
@@ -6,19 +6,19 @@ const inputParameters={
   'chargeRate':"Charge Rate"
  },
 german:{
-  'temperature':"Temperatur",
-  'soc':"Ladezustand",
-  'chargeRate':"Ladegeschwindigkeit"
+  'temperature':"Temperatur ",
+  'soc':"Ladezustand ",
+  'chargeRate':"Ladegeschwindigkeit "
  },
 kannada:{
-  'temperature':"ತಾಪಮಾನ",
-  'soc':"ಚಾರ್ಜ್ ಸ್ಥಿತಿ",
-  'chargeRate':"ಚಾರ್ಜ್ ದರ"
+  'temperature':"ತಾಪಮಾನ ",
+  'soc':"ಚಾರ್ಜ್ ಸ್ಥಿತಿ ",
+  'chargeRate':"ಚಾರ್ಜ್ ದರ "
  },
  hindi:{
   'temperature':"तापमान ",
   'soc': "चार्ज की स्थिति ",
-  'chargeRate': "चार्ज दर" 
+  'chargeRate': "चार्ज दर " 
  },
  spanish:{
   'temperature':"Temperatura",
@@ -53,10 +53,10 @@ hindi:{
      notOk: "रेंज से बाहर है :["
 },
 spanish:{
-    'peak': "se acerca al pico [!]",
-    'discharge': "se acerca a la descarga [!]",
-    'allOk': "está bien",
-     notOk: "está fuera de rango :["
+    'peak': " se acerca al pico [!]",
+    'discharge': " se acerca a la descarga [!]",
+    'allOk': " está bien",
+     notOk: " está fuera de rango :["
 }
 }
 module.exports={inputParameters, languages}
