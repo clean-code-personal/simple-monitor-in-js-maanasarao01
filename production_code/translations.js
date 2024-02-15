3fe4ecca7b82edca3ebd25f5d66727d5bee50b30
Maanasa_Rao <140826809+maanasarao01@users.noreply.github.com>
2024-02-15 17:35:40 +0530

    Create translations.js

---
+++ production_code/translations.js
@@ -0,0 +1,35 @@
+const languages={
+ english:{
+    peak:' is approaching peak [!]',
+    discharge:' is approaching discharge [!]',
+    allOk:' is Ok',
+    notOk:' is out of range :['
+  },
+ german:{
+    peak: "nähert sich dem Höhepunkt [!]",
+    discharge: "nähert sich der Entladung [!]",
+    allOk: "ist in Ordnung",
+    notOk: "ist außerhalb des Bereichs :["
+},
+ kannada:{
+ {
+    peak: "ಶಿಖರಕ್ಕೆ ಹತ್ತುತ್ತಿದೆ [!]",
+    discharge:"ಬಿಡುಗಡೆಯ ಹಂತಕ್ಕೆ ಬರುತ್ತಿದೆ [!]",
+    allOk: "ಸರಿಯಾಗಿದೆ",
+    notOk: "ಬಿಗಿಯಾಗಿದೆ :["
+ },
+hindi:{
+    peak: "शिखर के पास आ रहा है [!]",
+    discharge: "छूटने के पास आ रहा है [!]",
+    allOk: "ठीक है",
+    notOk: "रेंज से बाहर है :["
+
+},
+spanish:{
+    "peak": "se acerca al pico [!]",
+    "discharge": "se acerca a la descarga [!]",
+    "allOk": "está bien",
+    "notOk": "está fuera de rango :["
+}
+}
+module.exports={languages}
